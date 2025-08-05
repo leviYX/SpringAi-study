f52345037397606f8c6ae0efef8fbf2bd4de2191
liuwenqiang <wenqiang.liu@wenge.com>
2025-08-05 10:16:45 +0800

    advisor 和 common模块的抽取

---
+++ chat-memory/src/test/java/com/levi/springai/chatmemory/ChatMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi.springai.chatmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
