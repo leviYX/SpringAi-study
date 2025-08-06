d367b4d2ec8232aaabe3d0dd4b8e923556105624
liuwenqiang <wenqiang.liu@wenge.com>
2025-08-06 11:28:42 +0800

    结构化的支持

---
+++ structured-output/src/main/java/com/levi/springai/structuredoutput/StructuredOutputApplication.java
@@ -0,0 +1,13 @@
+package com.levi.springai.structuredoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuredOutputApplication.class, args);
+    }
+
+}
---
+++ structured-output/src/test/java/com/levi/springai/structuredoutput/StructuredOutputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi.springai.structuredoutput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredOutputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
