2640842846904dbfb5904e37c90a58414075b095
liuwenqiang <wenqiang.liu@wenge.com>
2025-08-07 08:22:41 +0800

    tools或者说functioncall的初始化

---
+++ tools-functioncall/src/main/java/com/levi/springai/toolsfunctioncall/ToolsFunctioncallApplication.java
@@ -0,0 +1,13 @@
+package com.levi.springai.toolsfunctioncall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsFunctioncallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsFunctioncallApplication.class, args);
+    }
+
+}
---
+++ tools-functioncall/src/test/java/com/levi/springai/toolsfunctioncall/ToolsFunctioncallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levi.springai.toolsfunctioncall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsFunctioncallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
